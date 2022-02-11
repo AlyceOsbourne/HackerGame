c5780f2dd52b28061e0b9a075080258483cd244a
Alyce <Alyceosbourne@outlook.com>
2022-02-11 18:10:42 +0000

    Fixed some feels, broken spacing etc

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # todo -> get __build_class__ working
-
+import tkinter.font
 from tkinter import *
 import builtins_restricted
 
-intro_text = "### Welcome to the Average System Shell ###\nTo help fry your brain without frying your computer\ncontrol enter to post code (yes this is temporary)"
+intro_text = "### Welcome to the Average System Shell ###\nTo help fry your brain without frying your computer\ncontrol enter to post code (yes this is temporary)\n"
 
 
 def logger_decorator(func):
@@ -23,11 +23,11 @@ class Shell:
         self.execute_input(self.grab_input())
 
     def grab_input(self) -> str:
-        text = self.input_text.get(1.0, END).rstrip("\\").rstrip().replace("'", "\'").replace('"', '\"')
+        text = self.input_text.get(1.0, END).lstrip().replace("'", "\'").replace('"', '\"').rstrip()
         if text.endswith("\\"):
             text = text[:-1]
-        text.replace("print_to_output(", "self.print_to_output(")
         self.input_text.delete(1.0, END)
+        self.input_text.mark_set("insert", "%d.%d" % (1., 1.))
         return text
 
     def execute_input(self, string: str):
@@ -35,21 +35,22 @@ class Shell:
             return
         try:
             eval_res = eval(compile(string, "<stdin>", "eval"), self.__globals__)
-            self.print_to_output(f"{string} = {eval_res}")
+            self.print_to_output(f">>> {string} == {eval_res}")
         except Exception:
             try:
                 exec(compile(string, "<stdin>", "exec"), self.__globals__)
-                self.print_to_output(string)
+                self.print_to_output(">>> "+string)
             except Exception as error:
-                self.print_to_output(string + "\n\t" + f"{error.__class__.__name__}: {error}")
+                self.print_to_output(">>> " + string + " -> " + f"{error.__class__.__name__}: {error}")
                 raise
         finally:
             print("\n")
 
-    def print_to_output(self, *string, sep="\n"):
+    def print_to_output(self, *string, sep=" "):
         self.output_text.config(state=NORMAL)
         for s in string:
             self.output_text.insert(END, s + sep)
+        self.output_text.insert(END, "\n")
         self.output_text.config(state=DISABLED)
 
     @staticmethod
@@ -61,11 +62,12 @@ class Shell:
     window.title("Shell")
     window.resizable(False, False)
     window.configure()
+    font = tkinter.font.Font()
     output_frame, input_frame = Frame(window), Frame(window)
     output_text = Text(output_frame, state=DISABLED, height=15, width=70, bg="black", fg="green")
     output_scroll = Scrollbar(output_frame, orient=VERTICAL, bg='black', command=output_text.yview)
     input_text = Text(input_frame, height=5, width=70, insertwidth=3, bg="black", fg="green",
-                      insertbackground="green")
+                      insertbackground="green", tabs=font.measure("    "))
     input_scroll = Scrollbar(input_frame, orient=VERTICAL, bg='black', command=input_text.yview)
     output_frame.pack(pady=3, padx=3)
     output_text.pack(side=LEFT)
