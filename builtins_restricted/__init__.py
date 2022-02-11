c4c5f748817457b99293183e69b52109188758c6
Alyce <Alyceosbourne@outlook.com>
2022-02-11 01:11:01 +0000

    Base work for interface

--- builtins_restricted/__init__.py
+++ builtins_restricted/__init__.py
@@ -1,5 +1,10 @@
 from builtins import *
 for i in  ["compile", "eval", "exec", "open", "input"]:
     del globals()[i]
+
 __all__ = [*globals().keys()]
+
+
+
+
 
--- main.py
+++ main.py
@@ -23,53 +23,60 @@ class Shell:
         self.execute_input(self.grab_input())
 
     def grab_input(self) -> str:
-        text = self.input_text.get(1.0, END)
+        text = self.input_text.get(1.0, END).replace("'", '"').replace('"', '\"').strip(" ")
+        text.replace("print(", "self.print(")
         self.input_text.delete(1.0, END)
         return text
 
     def execute_input(self, string: str):
-        if string.__contains__("import "):
-            pass  # todo special handling for import
-        elif string.__contains__("with open") or string.__contains__("open("):
-            pass  # todo special handling for open()
-        else:
+        try:
+            eval_res = eval(string, self.__globals__)
+            self.print(f"{string} = {eval_res}" + "\n\r")
+        except Exception:
             try:
-                self.update_output(f"{string} = {eval(string, self.__globals__)}" + "\n\r")
-            except Exception:
-                try:
-                    exec(string, self.__globals__)
-                    self.update_output(string + "\n\r")
-                except Exception as error:
-                    self.update_output(string + "\n\t" + f"{error.__class__.__name__}: {error}" + "\n\r")
-                    raise
-            self.update_output("\n\r")
+                exec(string, self.__globals__)
+                self.print(string + "\n\r")
+            except Exception as error:
+                self.print(string + "\n\t" + f"{error.__class__.__name__}: {error}" + "\n\r")
+                raise
 
-    def update_output(self, string):
+    def print(self, *string, sep="\n\r"):
         self.output_text.config(state=NORMAL)
-        self.output_text.insert(END, string)
+        for s in string:
+            self.output_text.insert(END, s+sep)
         self.output_text.config(state=DISABLED)
 
     def __init__(self, level=None):
         self.__globals__ = dict(**__game__globals__, **level) if level else __game__globals__
         self.window = Tk()
-        self.window.title("TB-RPG")
+        self.window.title("Shell")
+        self.window.resizable(False, False)
         self.window.configure()
         self.output_frame, self.input_frame = Frame(self.window), Frame(self.window)
         self.output_text = Text(self.output_frame, state=DISABLED, height=30, width=100, bg="black", fg="green")
-        self.output_scroll = Scrollbar(self.output_frame, orient=VERTICAL, bg='black')
+        self.output_scroll = Scrollbar(self.output_frame, orient=VERTICAL, bg='black', command=self.output_text.yview())
         self.input_text = Text(self.input_frame, height=10, width=100, insertwidth=3, bg="black", fg="green",
                                insertbackground="green")
-        self.input_scroll = Scrollbar(self.input_frame, orient=VERTICAL, bg='black')
+        self.input_scroll = Scrollbar(self.input_frame, orient=VERTICAL, bg='black', command=self.input_text.yview())
         self.output_frame.pack(pady=3, padx=3)
         self.output_text.pack(side=LEFT)
         self.output_scroll.pack(fill=Y, side=RIGHT)
         self.input_frame.pack()
         self.input_text.pack(side=LEFT)
         self.input_scroll.pack(fill=Y, side=RIGHT)
-        self.window.bind('<Control-Return>', self.update)
+        self.input_text.bind('<Control-Return>', self.update)
+
+        self.print(
+            """
+    Welcome to the Oops I Broke It Interactive Shell
+    Where you can play around, screw up, and not blow up your 
+    PC while you learn.   
+        """+"\n\r"
+        )
+
+
         self.window.mainloop()
 
-        self.update_output("Welcome to the Oops I Broke It Interactive Shell")
 
 
 if __name__ == "__main__":
