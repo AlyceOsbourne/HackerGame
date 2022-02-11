bf1e29fb43405b290806dace722ac2c125006121
Alyce <Alyceosbourne@outlook.com>
2022-02-11 14:08:15 +0000

    Base work for interface

--- builtins_restricted/__init__.py
+++ builtins_restricted/__init__.py
@@ -1,10 +1,38 @@
+import importlib
+
+#import os  # needs new implement
+#import sys  # needs new implement
+#import math
+#import json  # needs new implement
+#import itertools
+#import collections
+#import more_itertools
+
 from builtins import *
-for i in  ["compile", "eval", "exec", "open", "input"]:
+from builtins import __build_class__
+
+allowed_modules = ["itertools", "math", "collections"]
+
+def __import_module__(name, globals=None, locals=None, fromlist=(), level=0):
+    print("Attempting import", name)
+    if name in allowed_modules:
+        try:
+            return importlib.__import__(name, globals, locals, fromlist, level)
+        except ModuleNotFoundError as m:
+            print(m.name, m.msg)
+    else:
+        raise ModuleNotFoundError
+
+
+def __build_class__restricted__():
+    # todo class checks here, classes must not include outer access,
+    #  an in some instances, if does replace with custom function
+    return __build_class__
+
+for i in ["compile", "eval", "exec", "open", "input", "print"]:  # these deleted methods need recreation
     del globals()[i]
 
-__all__ = [*globals().keys()]
+globals()["__import__"] = __import_module__
+globals()["__build_class__"] = __build_class__restricted__
 
-
-
-
-
+__all__ = [k for k in globals().keys() if k not in ["allowed_modules"]]
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+# todo -> get __build_class__ working
+
 from tkinter import *
-from rich import print
+
 import builtins_restricted
+from rich import print
+
+intro_text = "### Welcome to the Average System Shell ###\n\rTo help fry your brain without frying your computer\n\r"
 
 
 def logger_decorator(func):
@@ -14,9 +19,6 @@ def logger_decorator(func):
     return out
 
 
-__game__globals__ = dict(__name__="Shell", __builtins__=builtins_restricted)
-
-
 class Shell:
 
     def update(self, *_):
@@ -24,59 +26,60 @@ class Shell:
 
     def grab_input(self) -> str:
         text = self.input_text.get(1.0, END).replace("'", '"').replace('"', '\"').strip(" ")
-        text.replace("print(", "self.print(")
+        text.replace("print_to_output(", "self.print_to_output(")
         self.input_text.delete(1.0, END)
         return text
 
     def execute_input(self, string: str):
         try:
             eval_res = eval(string, self.__globals__)
-            self.print(f"{string} = {eval_res}" + "\n\r")
+            self.print_to_output(f"{string} = {eval_res}")
         except Exception:
             try:
                 exec(string, self.__globals__)
-                self.print(string + "\n\r")
+                self.print_to_output(string)
             except Exception as error:
-                self.print(string + "\n\t" + f"{error.__class__.__name__}: {error}" + "\n\r")
+                self.print_to_output(string + "\n\t" + f"{error.__class__.__name__}: {error}")
                 raise
+        finally:
+            print("\n\r")
 
-    def print(self, *string, sep="\n\r"):
+    def print_to_output(self, *string, sep="\n\r"):
         self.output_text.config(state=NORMAL)
         for s in string:
-            self.output_text.insert(END, s+sep)
+            self.output_text.insert(END, s + sep)
         self.output_text.config(state=DISABLED)
 
+    @staticmethod
+    def import_module(self, *args):
+        self.print_to_output("Importing ", *args)
+        return None
+
+    window = Tk()
+    window.title("Shell")
+    window.resizable(False, False)
+    window.configure()
+    output_frame, input_frame = Frame(window), Frame(window)
+    output_text = Text(output_frame, state=DISABLED, height=30, width=100, bg="black", fg="green")
+    output_scroll = Scrollbar(output_frame, orient=VERTICAL, bg='black', command=output_text.yview())
+    input_text = Text(input_frame, height=10, width=100, insertwidth=3, bg="black", fg="green",
+                      insertbackground="green")
+    input_scroll = Scrollbar(input_frame, orient=VERTICAL, bg='black', command=input_text.yview())
+    output_frame.pack(pady=3, padx=3)
+    output_text.pack(side=LEFT)
+    output_scroll.pack(fill=Y, side=RIGHT)
+    input_frame.pack()
+    input_text.pack(side=LEFT)
+    input_scroll.pack(fill=Y, side=RIGHT)
+
+    __game__globals__ = dict(__name__="Shell", __builtins__=builtins_restricted, print=print_to_output)
+
     def __init__(self, level=None):
-        self.__globals__ = dict(**__game__globals__, **level) if level else __game__globals__
-        self.window = Tk()
-        self.window.title("Shell")
-        self.window.resizable(False, False)
-        self.window.configure()
-        self.output_frame, self.input_frame = Frame(self.window), Frame(self.window)
-        self.output_text = Text(self.output_frame, state=DISABLED, height=30, width=100, bg="black", fg="green")
-        self.output_scroll = Scrollbar(self.output_frame, orient=VERTICAL, bg='black', command=self.output_text.yview())
-        self.input_text = Text(self.input_frame, height=10, width=100, insertwidth=3, bg="black", fg="green",
-                               insertbackground="green")
-        self.input_scroll = Scrollbar(self.input_frame, orient=VERTICAL, bg='black', command=self.input_text.yview())
-        self.output_frame.pack(pady=3, padx=3)
-        self.output_text.pack(side=LEFT)
-        self.output_scroll.pack(fill=Y, side=RIGHT)
-        self.input_frame.pack()
-        self.input_text.pack(side=LEFT)
-        self.input_scroll.pack(fill=Y, side=RIGHT)
+        self.__globals__ = dict(**self.__game__globals__, **level, ) if level else self.__game__globals__
+        builtins_restricted.session = self
+        self.print_to_output(intro_text)
         self.input_text.bind('<Control-Return>', self.update)
-
-        self.print(
-            """
-    Welcome to the Oops I Broke It Interactive Shell
-    Where you can play around, screw up, and not blow up your 
-    PC while you learn.   
-        """+"\n\r"
-        )
-
-
         self.window.mainloop()
-
 
 
 if __name__ == "__main__":
